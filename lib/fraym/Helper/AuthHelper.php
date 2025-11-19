b4f2459b51d86be0a268c5f83296b8c6915b275a
Alex Garshin <alxgarshin@gmail.com>
2025-11-19 12:55:39 -0300

    AI-65 PASSWORD_ARGON2ID fix

--- lib/fraym/Helper/AuthHelper.php
+++ lib/fraym/Helper/AuthHelper.php
@@ -88,7 +88,7 @@ abstract class AuthHelper implements Helper
             $password = self::addProjectHashWord($password);
         }
 
-        return password_hash($password, PASSWORD_ARGON2ID, [
+        return password_hash($password, \PASSWORD_ARGON2ID, [
             'memory_cost' => 1 << 17,
             'time_cost'   => 3,
             'threads'     => 1,
