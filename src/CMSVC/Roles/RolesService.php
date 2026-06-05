9969771f34d1238e81983c53a71493edc0449af4
Alex Garshin <alxgarshin@gmail.com>
2026-06-05 10:30:15 -0300

    AI-190 Roles service fixes

--- src/CMSVC/Roles/RolesService.php
+++ src/CMSVC/Roles/RolesService.php
@@ -523,7 +523,10 @@ class RolesService extends BaseService
                         ) {
                         } else {
                             $this->rolesDataArray[$i] = $roleData;
-                            $charactersGroupsToRolesData[$roleData['group_id']][$i] = $roleData['character_id'] ?? null;
+
+                            if ($roleData['group_id'] ?? false) {
+                                $charactersGroupsToRolesData[$roleData['group_id']][$i] = $roleData['character_id'] ?? null;
+                            }
                             ++$i;
                         }
                     }
@@ -538,32 +541,35 @@ class RolesService extends BaseService
                                 /* проверяем, является ли группа родительской для данного инстанса персонажа, и видна ли она */
                                 if (($roleData['group_id'] ?? false) && $roleData['group_id'] !== '') {
                                     $idInProjectGroupsList = $projectGroupsListIdToKey[$roleData['group_id']];
-                                    $projectGroupData = $this->projectGroupsList[$idInProjectGroupsList];
-                                    $level = $projectGroupData[2];
 
-                                    while ($level > 0 && $idInProjectGroupsList > 0) {
-                                        --$idInProjectGroupsList;
-                                        $projectGroupData = $this->projectGroupsList[$idInProjectGroupsList] ?? [];
+                                    if ($idInProjectGroupsList !== null) {
+                                        $projectGroupData = $this->projectGroupsList[$idInProjectGroupsList];
+                                        $level = $projectGroupData[2];
 
-                                        if ($projectGroupData && $projectGroupData[2] < $level) {
-                                            $level = $projectGroupData[2];
+                                        while ($level > 0 && $idInProjectGroupsList > 0) {
+                                            --$idInProjectGroupsList;
+                                            $projectGroupData = $this->projectGroupsList[$idInProjectGroupsList] ?? [];
 
-                                            if (($projectGroupData[0] ?? false) && ($projectGroupData[0] ?? false) && is_array($charactersGroupsToRolesData[$projectGroupData[0]] ?? false)) {
-                                                while (
-                                                    in_array(
-                                                        $roleData['character_id'],
-                                                        $charactersGroupsToRolesData[$projectGroupData[0]],
-                                                        true,
-                                                    )
-                                                ) {
-                                                    $keyInRolesDataArray = array_search(
-                                                        $roleData['character_id'],
-                                                        $charactersGroupsToRolesData[$projectGroupData[0]],
-                                                        true,
-                                                    );
-                                                    unset($this->rolesDataArray[$keyInRolesDataArray]['character_id']);
-                                                    unset($charactersGroupsToRolesData[$projectGroupData[0]][$keyInRolesDataArray]);
-                                                    // $this->rolesDataArray[$keyInRolesDataArray]['character_id'] = 'do_not_show';
+                                            if ($projectGroupData && $projectGroupData[2] < $level) {
+                                                $level = $projectGroupData[2];
+
+                                                if (($projectGroupData[0] ?? false) && ($projectGroupData[0] ?? false) && is_array($charactersGroupsToRolesData[$projectGroupData[0]] ?? false)) {
+                                                    while (
+                                                        in_array(
+                                                            $roleData['character_id'],
+                                                            $charactersGroupsToRolesData[$projectGroupData[0]],
+                                                            true,
+                                                        )
+                                                    ) {
+                                                        $keyInRolesDataArray = array_search(
+                                                            $roleData['character_id'],
+                                                            $charactersGroupsToRolesData[$projectGroupData[0]],
+                                                            true,
+                                                        );
+                                                        unset($this->rolesDataArray[$keyInRolesDataArray]['character_id']);
+                                                        unset($charactersGroupsToRolesData[$projectGroupData[0]][$keyInRolesDataArray]);
+                                                        // $this->rolesDataArray[$keyInRolesDataArray]['character_id'] = 'do_not_show';
+                                                    }
                                                 }
                                             }
                                         }
