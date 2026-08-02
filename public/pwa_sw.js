66c3516783916b82cb8897d6a12d92f050df786c
Alex Garshin <alxgarshin@gmail.com>
2026-08-02 16:05:52 -0300

    AI-205 Group select in myapplication fix

--- public/pwa_sw.js
+++ public/pwa_sw.js
@@ -1,5 +1,5 @@
 // cacheName for cache versioning
-const cacheName = '20250905_0900';
+const cacheName = '20250905_1000';
 
 self.addEventListener('install', function (event) {
     // Всегда активировать новый SW немедленно, не ждать закрытия вкладок
